f9f1b6e4a45107ceb3415637a83b7d43feb6ac81
George Morev <morevgeorge@gmail.com>
2025-03-20 14:41:46 +0300

    Рефакторинг кода и новый аллюр-отчет

--- pages/home_page.py
+++ pages/home_page.py
@@ -39,3 +39,23 @@ class HomePage(BasePage):
         locator = (HomePageLocators.ORDER_BUTTON_HEADER if position == 'header'
                    else HomePageLocators.ORDER_BUTTON_FOOTER)
         self.click_element(locator)
+
+    @allure.step("Возвращает текущий URL страницы")
+    def get_current_url(self):
+        return self.driver.current_url
+
+    @allure.step("Возвращает дескриптор текущего окна браузера")
+    def get_current_window_handle(self):
+        return self.driver.current_window_handle
+
+    @allure.step("Переключиться на вкладку")
+    def switch_to_window(self, window_handle):
+        self.driver.switch_to.window(window_handle)
+
+    @allure.step("Закрыть текущую вкладку и вернуться в исходное окно")
+    def close_current_tab_and_switch_back(self, original_window):
+        if len(self.driver.window_handles) > 1:
+            self.driver.close()
+            self.driver.switch_to.window(original_window)
+            allure.attach(self.driver.current_url, name="Restored Window URL",
+                          attachment_type=allure.attachment_type.TEXT)
--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators.base_page_locators import BasePageLocators
 
+
 @allure.feature('Тесты навигации через логотипы')
 class TestLogoNavigation:
     @allure.title('Переход через логотип Самоката')
@@ -10,9 +11,9 @@ class TestLogoNavigation:
     def test_scooter_logo_navigation(self, home_page, driver):
         with allure.step('Кликнуть на логотип Самоката'):
             home_page.click_element(BasePageLocators.SCOOTER_LOGO)
-        
+
         with allure.step('Проверить URL после перехода'):
-            current_url = driver.current_url
+            current_url = home_page.get_current_url()
             allure.attach(current_url, name="Current URL", attachment_type=allure.attachment_type.TEXT)
             assert current_url == "https://qa-scooter.praktikum-services.ru/"
 
@@ -20,8 +21,8 @@ class TestLogoNavigation:
     @allure.description('Проверка открытия новой вкладки с Дзеном через логотип Яндекса')
     def test_yandex_logo_navigation(self, home_page, driver):
         with allure.step('Запомнить текущее окно браузера'):
-            original_window = driver.current_window_handle
-        
+            original_window = home_page.get_current_window_handle()
+
         with allure.step('Кликнуть на логотип Яндекса'):
             home_page.click_element(BasePageLocators.YANDEX_LOGO)
 
@@ -30,7 +31,7 @@ class TestLogoNavigation:
                 new_window = home_page.wait_for_new_tab(driver, original_window)
 
             with allure.step('Переключиться на новую вкладку'):
-                driver.switch_to.window(new_window)
+                home_page.switch_to_window(new_window)
 
                 with allure.step('Проверить URL в новой вкладке'):
                     current_url = home_page.wait_for_url_in_new_tab(driver, "dzen.ru")
@@ -38,7 +39,4 @@ class TestLogoNavigation:
 
         finally:
             with allure.step('Закрыть новую вкладку и вернуться в исходное окно'):
-                if len(driver.window_handles) > 1:
-                    driver.close()
-                    driver.switch_to.window(original_window)
-                    allure.attach(driver.current_url, name="Restored Window URL", attachment_type=allure.attachment_type.TEXT)
+                home_page.close_current_tab_and_switch_back(original_window)
